b83eccb292c15fd23c2058458ecd9ed78db48cc0
fahadullah <fahimfahadullah@outlook.com>
2026-01-24 12:46:19 +0600

    hypr ecosystem tools added in the script for installtion

--- setup.fish
+++ setup.fish
@@ -1,9 +1,41 @@
 #!/usr/bin/fish
+set packages \
+    hyprland\
+    kitty\
+    wofi\
+    nautilus\
+    hyprpaper \
+    hyprpicker \
+    hyprlauncher \
+    hypridle \
+    hyprlock \
+    xdg-desktop-portal-hyprland \
+    hyprsysteminfo \
+    hyprsunset \
+    hyprpolkitagent \
+    hyprland-qt-support \
+    hyprqt6engine \
+    hyprpwcenter \
+    hyprshutdown \
+    hyprtoolkit \
+    hyprcursor \
+    hyprutils \
+    hyprlang \
+    hyprwayland-scanner \
+    aquamarine \
+    hyprgraphics \
+    hyprland-guiutils
 
-set PKGS hyprland hyprpaper kitty wofi nautilus
 
-for pkg in $PKGS
-    echo "Installing $pkg"
-    sudo pacman -S --noconfirm --needed $pkg
+sudo pacman -Sy
+
+for pkg in $packages
+    if pacman -Si $pkg > /dev/null 2>&1
+        echo "Installing $pkg..."
+        sudo pacman -S --needed --noconfirm $pkg
+    else
+        echo ">> Warning: '$pkg' not in the official repositories."
+    end
 end
-echo "Iterating over package list done"
+
+echo "Complete"
